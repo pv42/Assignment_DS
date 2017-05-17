63f1828f2b09113f5e7a0ba956b05266ca502e42
pv42 <pv42.97@gmail.com>
2017-05-17 19:51:12 +0200

    added moles and pengpenguins

---
+++ src/assingmentDS/animal/Mole.java
@@ -0,0 +1,11 @@
+package assingmentDS.animal;
+
+/**
+ * Created by pv42 on 17.05.2017.
+ * Ein Mull (nicht giftig, kann nicht fliegen, kein Raubtier, kein Aasfresser)
+ */
+public class Mole extends LandMammal{
+    Mole(String name) {
+        super(name, "Mole", false, false, false, 0.09, false);
+    }
+}
---
+++ src/assingmentDS/animal/Penguin.java
@@ -0,0 +1,11 @@
+package assingmentDS.animal;
+
+/**
+ * Created by pv42 on 17.05.2017.
+ * Ein Pinguin (nicht giftig, kann nicht fliegen, Raubtier, kein Aasfresser)
+ */
+public class Penguin extends Bird{
+    Penguin(String name) {
+        super(name, "Penguin", false, false, true, 0.7, false);
+    }
+}
