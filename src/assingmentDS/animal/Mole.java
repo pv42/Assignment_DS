c30f047251a97c5fb3ae8d0e2ff63267a55a09fa
pv42 <pv42.97@gmail.com>
2017-05-18 13:56:24 +0200

    finalised

--- src/assingmentDS/animal/Mole.java
+++ src/assingmentDS/animal/Mole.java
@@ -4,7 +4,7 @@ package assingmentDS.animal;
  * Created by pv42 on 17.05.2017.
  * Ein Mull (nicht giftig, kann nicht fliegen, kein Raubtier, kein Aasfresser)
  */
-public class Mole extends LandMammal{
+public final class Mole extends LandMammal{
     Mole(String name) {
         super(name, "Mole", false, false, false, 0.09, false);
     }
