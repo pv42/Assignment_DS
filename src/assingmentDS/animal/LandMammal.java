66768b09ffa9fdac43a36af76362d06eec3bd10a
pv42 <pv42.97@gmail.com>
2017-05-19 01:31:15 +0200

    added generic mammals

--- src/assingmentDS/animal/LandMammal.java
+++ src/assingmentDS/animal/LandMammal.java
@@ -4,7 +4,7 @@ package assingmentDS.animal;
  * Created on 24.04.2017.
  * Ein Landsäugetier (lebt nicht Unterwasser)
  */
-public abstract class LandMammal extends Animal {
+public abstract class LandMammal extends Mammal {
 
     LandMammal(String name, String species, boolean isPoisonous, boolean mayFly, boolean isPredator, double avgSize, boolean isScavenger) {
         super(name, species, isPoisonous, mayFly, false, isPredator, avgSize, isScavenger);
