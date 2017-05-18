4851c060cc7a683112ba4a482673e4e2c15fa580
pv42 <pv42.97@gmail.com>
2017-05-19 01:32:26 +0200

    abstraced

--- src/assingmentDS/animal/Mammal.java
+++ src/assingmentDS/animal/Mammal.java
@@ -3,7 +3,7 @@ package assingmentDS.animal;
 /**
  * Created by pv42 on 18.05.2017.
  */
-public class Mammal extends Animal {
+public abstract class Mammal extends Animal {
     Mammal(String name, String species, boolean isPoisonous, boolean mayFly, boolean isLivingSubmerged, boolean isPredator, double avgSize, boolean isScavenger) {
         super(name, species, isPoisonous, mayFly, isLivingSubmerged, isPredator, avgSize, isScavenger);
     }
