29b565f0d38e7d3ad3e733be8eafadef6a0de2a3
pv42 <pv42.97@gmail.com>
2017-05-19 22:04:46 +0200

    .

--- src/assingmentDS/util/MyLinkedList.java
+++ src/assingmentDS/util/MyLinkedList.java
@@ -1,163 +1,171 @@
 package assingmentDS.util;
 
-//Hinweis: tabs sollten durch Leerzeichen ersetzt werden
-//todo Kommentare!!!!
-
+import java.util.AbstractList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-import java.util.function.Consumer;
 
-public class MyLinkedList<T> {
-	
-	private class Node {
-		private Node next = null;
-		private T data;
-		public Node(T d) {
-			data = d;
-		}
-		
-		public void setNext(Node next){
-			this.next = next;
-		}
-		
-		public Node getNext(){
-			return this.next;
-		}
-		
-		public void setData(T data){
-			this.data = data;
-		}
-		
-		public T getData(){
-			return this.data;
-		}
-	}
-	
-	private Node head;
-	
-	public MyLinkedList() {
-		head = null;
-	}
+public class MyLinkedList<T> extends AbstractList<T> {
 
-	@Override
-	public String toString() { //todo besser währe eine Methode, die eine String zurückgibt, der dann ausgegehen werden kann vgl. public String toString()
-		Node current = head;
-		String str = "";
-		while (current.getNext() != null) {
-			str = str + current.getData() + " -> ";
-			current = current.getNext();
-		}
-		str = str + current.getData();
-		return str;
-	}
-	
-	public int size(){
-		Node current = head;
-		int size = 0;
-		while (current.getNext() != null) {
-			size++;
-			current = current.getNext();
-		}
-		return size;
-	}
-	
-	public boolean add(T data) {
-		Node end = new Node(data);
-		Node current = head;
+    private class Node {
+        private Node next = null;
+        private T data;
+        Node(T d) {
+            data = d;
+        }
 
-		while (current.getNext() != null) {
-			current = current.getNext();
-		}
-		current.setNext(end);
-		return true;
-	}
+        void setNext(Node next){
+            this.next = next;
+        }
 
-	public boolean add(T data, int index) {
-		Node node = new Node(data); //todo end ist ein unpassender Name
-		Node current = head;
-		int jump;
+        Node getNext(){
+            return this.next;
+        }
 
-		if ((index > size()) || (index < 1)) { //todo indizierung bei Listen immer mit 0 beginnend!
-			return false;
-		} else {
-			jump = 0;
-			while (jump < index - 1) {
-				current = current.getNext();
-				jump++;
-			}
-			node.setNext(current.getNext());
-			current.setNext(node);
-			return true;
-		}
-	}
+        T getData(){
+            return this.data;
+        }
+    }
 
-	public boolean remove(T data) { //todo in public boolean remove(T data) ändern
-		Node current = head;
-		while (current.getNext() != null) {
-			if (current.getNext().getData() == data) {
-				current.setNext(current.getNext().getNext());
-				return true;
-			}
-			current = current.getNext();
-		}
-		return false;
-	}
+    private class MyListIterator implements Iterator<T>{
+        private int index = 0;
+        int listSize = size();
 
-	public boolean remove(int index) { //todo in public boolean remove(int index) ändern
-		Node current = head;
-		int jump;
-		if ((index > size()) || (index < 1)) { // index von 0 bis list.getSize() -1
-			return false;
-		} else {
-			jump = 0;
-			while (jump < index - 1) {
-				current = current.getNext();
-				jump++;
-			}
-			current.setNext(current.getNext().getNext());
-			return true;
-		}
-	}
-	
-	public T get(int index){
-		Node current = head;
-		for (int i = 0; i < index; i++){
-			current = current.getNext();
-		}
-		return current.getData();
+        public boolean hasNext(){
+            return index < listSize;
+        }
 
-	}
+        public T next(){
+            if (!hasNext()) throw new NoSuchElementException();
+            index++;
+            return current();
+        }
 
-	public Iterator<T> iterator(){
-		return new Iterator<T>(){
-			private int index = 0;
-			int listSize = size();
+        public T current() {
+            return get(index);
+        }
+    }
 
-			public boolean hasNext(){
-				return index < listSize;
-			}
+    private Node head;
 
-			public T next(){
-				if (!hasNext()) throw new NoSuchElementException();
-				index++;
-				return current();
-			}
-			public void remove(){
-				throw new UnsupportedOperationException();
-			}
+    public MyLinkedList() {
+        head = null;
+    }
 
-			void forEachRemaining (Consumer<T> action){
-				while (hasNext()){
-					action.accept(next());
-				}
-			}
+    @Override
+    public String toString() {
+        Node current = head;
+        String str = "";
+        while (current.getNext() != null) {
+            str = str + current.getData() + " -> ";
+            current = current.getNext();
+        }
+        str = str + current.getData();
+        return str;
+    }
 
-			public T current() {
-				return get(index);
-			}
-		};
-	}
-	
-	//todo Eine get(int index) Methode ist dringend erforderlich
-	// wenn die Interface List (https://docs.oracle.com/javase/8/docs/api/java/util/List.html) erfüllt würde währe super aber nicht notwendig
+    public int size(){
+        Node current = head;
+        int size = 0;
+        while (current != null) {
+            size++;
+            current = current.getNext();
+        }
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public boolean add(T data) {
+        Node end = new Node(data);
+        if(head == null) {
+            head = end;
+            return true;
+        }
+        Node current = head;
+        while (current.getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(end);
+        return true;
+    }
+
+    public boolean add(T data, int index) {
+        if (index < 0) return false;
+        Node node = new Node(data);
+        Node current = head;
+        int jump = 0;
+        while (jump < index ) {
+            if(current == null) return false;
+            current = current.getNext();
+            jump++;
+        }
+        if (current == null) {
+            head = node;
+        } else {
+            current.setNext(node);
+            node.setNext(current.getNext());
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object data) {
+        Node current = head;
+        while (current.getNext() != null) {
+            if (current.getNext().getData().equals(data)) {
+                current.setNext(current.getNext().getNext());
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        Node current = head;
+        int jump;
+        if ((index >= size()) || (index < 0)) {
+            return null;
+        } else {
+            jump = 0;
+            while (jump < index) {
+                current = current.getNext();
+                if(current == null) return null;
+                jump++;
+            }
+            if(current.getNext() != null) {
+                current.setNext(current.getNext().getNext());
+                return current.getNext().getData();
+            }
+            return null;
+
+        }
+    }
+
+    public T get(int index){
+        Node current = head;
+        for (int i = 0; i < index; i++){
+            current = current.getNext();
+        }
+        return current.getData();
+
+    }
+
+    @Override
+    public MyListIterator iterator(){
+        return new MyListIterator();
+    }
+
+    @Override
+    public void clear() {
+        head = null;
+    }
+
+
+    // wenn die Interface List (https://docs.oracle.com/javase/8/docs/api/java/util/List.html) erfüllt würde währe super aber nicht notwendig
 }
