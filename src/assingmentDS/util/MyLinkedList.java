6b5ce3cd5f75c701714746747fd791e54e228239
locjul <juliuslochbaum@googlemail.com>
2017-05-19 11:44:32 +0200

    Update MyLinkedList.java

--- src/assingmentDS/util/MyLinkedList.java
+++ src/assingmentDS/util/MyLinkedList.java
@@ -5,41 +5,55 @@ package assingmentDS.util;
 
 public class MyLinkedList<T> {
 	
-	private class Node<T> { //todo das sollte eine privarte Unterklasse von MyLinkedList sein, dann währe in dieser Klasse <T> auch unnötig
-		Node next = null; //todo das sollte privat sein und mit gettern und settern realisiert sein!
-		T data;
+	private class Node<T> {
+		private Node next = null;
+		private T data;
 		public Node(T d) {
 			data = d;
+		}
+		
+		public void setNext(Node next){
+			this.next = next;
+		}
+		
+		public Node getNext(){
+			return this.next;
+		}
+		
+		public void setData(T data){
+			this.data = data;
+		}
+		
+		public Node getData(){
+			return this.data;
 		}
 	}
 	
 	private Node head;
-	private int listCount; //todo der counter ist buganfällig besser währe eine rekursive Längenzälung
-
+	
 	public MyLinkedList() {
-		head = new Node(0); //todo eine neue (= leere) List sollte keine Elemente enthalten
+		head = null;
 		listCount = 0;
 	}
 
-	public void show() { //todo besser währe eine Methode, die eine String zurückgibt, der dann ausgegehen werden kann vgl. public String toString()
+	@Override
+	public String toString() { //todo besser währe eine Methode, die eine String zurückgibt, der dann ausgegehen werden kann vgl. public String toString()
 		Node current = head;
-		while (current.next != null) {
-			System.out.print(current.data + " -> ");
-			current = current.next;
+		String str = "";
+		while (current.getNext() != null) {
+			str = str + current.getData() + " -> ";
+			current = current.getNext();
 		}
-		System.out.println(current.data);
+		str = str + current.getData();
+		return str;
 	}
-
-	public int getSize() {
-		return listCount;
-	} //todo besser währe eine richtige Zählung der Elemente
-
+	
 	public int size(){
 		Node current = head;
 		int size = 0;
-		while (current.next != null) {
+		while (current.getNext() != null) {
 			size++;
-			current = current.next;
+			current = current.getNext();
 		}
 		return size;
 	}
@@ -48,11 +62,10 @@ public class MyLinkedList<T> {
 		Node end = new Node(data);
 		Node current = head;
 
-		while (current.next != null) {
-			current = current.next;
+		while (current.getNext() != null) {
+			current = current.getNext();
 		}
-		current.next = end;
-		listCount++;
+		current.setNext(end);
 		return true;
 	}
 
@@ -61,35 +74,33 @@ public class MyLinkedList<T> {
 		Node current = head;
 		int jump;
 
-		if ((index > listCount) || (index < 1)) { //todo indizierung bei Listen immer mit 0 beginnend!
+		if ((index > size()) || (index < 1)) { //todo indizierung bei Listen immer mit 0 beginnend!
 			return false;
 		} else {
 			jump = 0;
 			while (jump < index - 1) {
-				current = current.next;
+				current = current.getNext();
 				jump++;
 			}
-			node.next = current.next;
-			current.next = node;
-			listCount++;
+			node.setNext(current.getNext());
+			current.setNext(node);
 			return true;
 		}
 	}
 
-	public boolean deleteNodeWithData(T d) { //todo in public boolean remove(T data) ändern
+	public boolean remove(T data) { //todo in public boolean remove(T data) ändern
 		Node current = head;
-		while (current.next != null) {
-			if (current.next.data == d) {
-				current.next = current.next.next;
-				listCount--;
+		while (current.getNext() != null) {
+			if (current.getNext().getData() == data) {
+				current.setNext(current.getNext().getNext());
 				return true;
 			}
-			current = current.next;
+			current = current.getNext();
 		}
 		return false;
 	}
 
-	public boolean deleteNodeAtIndex(int index) { //todo in public boolean remove(int index) ändern
+	public boolean remove(int index) { //todo in public boolean remove(int index) ändern
 		Node current = head;
 		int jump;
 		if ((index > listCount) || (index < 1)) { // index von 0 bis list.getSize() -1
@@ -97,14 +108,18 @@ public class MyLinkedList<T> {
 		} else {
 			jump = 0;
 			while (jump < index - 1) {
-				current = current.next;
+				current = current.getNext();
 				jump++;
 			}
-			current.next = current.next.next;
-			listCount--;
+			current.setNext(current.getNext().getNext());
 			return true;
 		}
 	}
+	
+	public Node get(int index){
+		
+	}
+	
 	//todo Eine get(int index) Methode ist dringend erforderlich
 	// wenn die Interface List (https://docs.oracle.com/javase/8/docs/api/java/util/List.html) erfüllt würde währe super aber nicht notwendig
 }
